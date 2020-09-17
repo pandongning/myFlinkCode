b1821256dfa2a4a14ee97ea60e471b3ee96ff6f1
pdn <pandongning@baijiahulian.com>
2020-09-17 21:23:32 +0800

    pdn第4次提交20200917

--- src/main/java/DemoOne.java
+++ src/main/java/DemoOne.java
@@ -1,9 +1,6 @@
 
 public class DemoOne {
     public static void main(String[] args) {
-        double i = 1599990790000.0 - ((1599990790000.0 - 28800000 + 2000.0) % 2000.0);
-        double j = (1599990790000.0 - 28800000 + 2000.0) % 2000;
-        System.out.println(i);
-        System.out.println(j);
+        System.out.println("aaa");
     }
 }
